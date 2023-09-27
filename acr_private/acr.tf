88c334e9d78213bca6c88d84cd21a7ffbd122d68
aselvara <aakashmay26@gmail.com>
2023-09-27 19:18:36 -0400

    ACR done

--- acr_private/acr.tf
+++ acr_private/acr.tf
@@ -6,6 +6,14 @@ data "terraform_remote_state" "network" {
   }
 }
 
+data "terraform_remote_state" "bastion_host" {
+  backend = "local"
+
+  config = {
+    path = "../bastion_host/terraform.tfstate"
+  }
+}
+
 locals {
   virtual_network = {
     name           = data.terraform_remote_state.network.outputs.vnet_name
@@ -33,3 +41,18 @@ module "acr" {
   vnet_rg = local.virtual_network.resource_group
   tags  = local.tags
 }
+
+# Providing access to Bastion VM to pull images from ACR
+resource "azurerm_role_assignment" "acr_pull_bastion_host" {
+  scope                = module.acr.acr_id
+  role_definition_name = "AcrPull"
+  principal_id         = data.terraform_remote_state.bastion_host.outputs.bastion_vm_identity[0].principal_id
+}
+
+# Providing access to Bastion VM to push images to ACR
+resource "azurerm_role_assignment" "acr_push_bastion_host" {
+  scope                = module.acr.acr_id
+  role_definition_name = "AcrPush"
+  principal_id         = data.terraform_remote_state.bastion_host.outputs.bastion_vm_identity[0].principal_id
+}
+
